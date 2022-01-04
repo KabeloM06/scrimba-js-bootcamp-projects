ba437b117561d42fbdeba7a3293673c0b2fb804c
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-01-04 16:07:31 +0200

    first conditional challenge solutions added

--- first-conditional-challenge/index.js
+++ first-conditional-challenge/index.js
@@ -4,3 +4,29 @@
 
 const time = "morning" // could be "afternoon", "evening" and "night"
 
+// challenge 1
+if (time === "morning") {
+    console.log("good morning")
+} else if (time === "afternoon") {
+    console.log("good afternoon")
+} else if (time === "evening") {
+    console.log("good evening")
+} else {
+    console.log("good night")
+}
+
+// challenge 2
+
+// switch (time) {
+//     case "morning":
+//         console.log("good morning")
+//         break
+//     case "afternoon":
+//         console.log("good afternoon")
+//         break
+//     case "evening":
+//         console.log("good evening")
+//         break
+//     default:
+//         console.log("good night")
+// }
