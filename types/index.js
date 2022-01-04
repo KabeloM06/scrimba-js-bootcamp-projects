34e754163bc07760ab2beb9ac0d1e1987a12a58c
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-01-04 20:25:23 +0200

    tyyes challenge solutions

--- types/index.js
+++ types/index.js
@@ -2,21 +2,27 @@
 // What will the following console logs display? (they're all true and false)
 
 console.log("Challenge 1:")
-console.log(!undefined);
-console.log(Boolean(NaN)); 
-console.log(false === false);
-console.log(5 === "5");
-console.log("Hello" == "hello");
+console.log(!undefined); //true
+console.log(Boolean(NaN)); //false
+console.log(false === false); //true
+console.log(5 === "5"); //false
+console.log("Hello" == "hello"); //false
 
 // Challenge 2:
 // What will the following console logs display? (they're all true and false)
 console.log("Challenge 2:")
-console.log(Boolean(0));
-console.log(Boolean("0"));
-console.log(Boolean(""));
-console.log(!null);
-console.log(!!"hello");
+console.log(Boolean(0)); //false
+console.log(Boolean("0")); //true
+console.log(Boolean("")); //false
+console.log(!null); // true
+console.log(!!"hello"); //true
 
 // Challenge 3:
 // List all the falsy values in JavaScript
 
+// NaN
+// 0
+// null
+// false
+// ""
+// undefined
