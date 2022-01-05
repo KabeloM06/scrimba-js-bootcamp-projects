3f7d8275aa389d334d3cb91d0b3444fc6bccb6a2
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-01-05 18:28:21 +0200

    arrow function challenge solution

--- arrow-challenge/index.js
+++ arrow-challenge/index.js
@@ -3,26 +3,21 @@
 
 
 // Challenge start 
-function splitBill(amount, numPeople) {
-    return `Each person needs to pay ${amount / numPeople}`
-}
+splitBill = (amount, numPeople) => `Each person needs to pay R${amount / numPeople}`;
 
-// console.log(splitBill(10, 2));
-// console.log(splitBill(10, 4));
-// console.log(splitBill(10, 5));
+console.log(splitBill(10, 2));
+console.log(splitBill(10, 4));
+console.log(splitBill(10, 5));
 
 
 // Stretch goal start
-function countdown(startingNumber, step) {
+countdown = (startingNumber, step) => {
   let countFromNum = startingNumber + step;
-  return function decrease() {
-    countFromNum -= step;
-    return countFromNum;
-  }
+  return () => countFromNum -= step;
 }
 
-// const countingDown = countdown(20, 2);
+const countingDown = countdown(20, 2);
 
-// console.log(countingDown());
-// console.log(countingDown());
-// console.log(countingDown());
+console.log(countingDown());
+console.log(countingDown());
+console.log(countingDown());
