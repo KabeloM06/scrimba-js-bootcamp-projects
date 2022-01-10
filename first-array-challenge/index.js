a92a1034c0dd76f9bbef90c696de7d63d04c8c63
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-01-10 09:51:26 +0200

    first array challenge solutions

--- first-array-challenge/index.js
+++ first-array-challenge/index.js
@@ -3,3 +3,18 @@
 // 2. Log out the last song in the array, and make it so that your solution will log out the last one regardless of how many items there are in the array
 // 3. Invoke the pop method on your created array. What values do you have left in the array? 
 
+const favouriteSongs = [
+
+]
+
+favouriteSongs.push("Confessions");
+favouriteSongs.push("Stan");
+favouriteSongs.push("Ave Maria");
+
+
+
+console.log(`log out last array: ${favouriteSongs[favouriteSongs.length-1]}`);
+
+favouriteSongs.pop()
+
+console.log(`log out last array after pop: ${favouriteSongs[favouriteSongs.length-1]}`);
