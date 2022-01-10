a36a03b3784e6114b1874e9dce8aaabfa2b7fd84
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-01-10 13:34:16 +0200

    transform array with reduce solution

--- transform-array-with-reduce/index.js
+++ transform-array-with-reduce/index.js
@@ -11,4 +11,20 @@ const cars = [
     { name: "Tesla", isElectric: true, weight: 1750 },
     { name: "BMW", isElectric: true, weight: 1350 },  
   ];
+
+  const sumWeights = cars.reduce((accumulator, carWeight) => {
+      return accumulator + carWeight.weight;
+  }, 0)
+
+  console.log(`The total weight is ${sumWeights}`);
   
+  const sumElectWeights = cars.reduce((accumulator, car) => {
+    if (car.isElectric) {
+        return accumulator + car.weight;
+    } else {
+        return accumulator
+    }
+    
+  }, 0)
+
+  console.log(`The weight of only the electric cars is ${sumElectWeights}`);
