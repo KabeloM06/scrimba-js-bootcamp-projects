8e44bdea98c80d0b25dfccadbe9233abd0e3b25f
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-01-14 11:42:22 +0200

    create and modify elements challenge solutions

--- challenge-create-and-modify-elements/index.js
+++ challenge-create-and-modify-elements/index.js
@@ -2,3 +2,13 @@
 // 1. Update text in the Scrimba mini-browser to match the title of this cast
 // 2. Create an h2 with class 'tagline' and text "I can create HTML elements!"
 // Add it right under the modified text.
+const letsChange = document.querySelector('h1');
+letsChange.innerHTML = " Challenge: Creating and Modifying HTML Elements";
+
+const newElement = document.createElement("h2");
+newElement.className = 'tagline';
+newElement.innerHTML = "I can create HTML Elements";
+
+const post = document.querySelector('tagline');
+
+letsChange.append(newElement);
