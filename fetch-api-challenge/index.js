eda9e101a09dfa66a5263a7ea202535a4705a6b8
KabeloM06 <69459421+KabeloM06@users.noreply.github.com>
2022-01-17 11:00:07 +0200

    Fetch API challenge solution

--- fetch-api-challenge/index.js
+++ fetch-api-challenge/index.js
@@ -5,3 +5,15 @@
 // Handle errors if something does not quite work. 
 
 // Here's the endpoint: https://jsonplaceholder.typicode.com/users/3
+
+fetch("https://jsonplaceholder.typicode.com/users/3")
+.then(response => {
+    if (!response.ok) {
+        throw new Error(response.status);
+    }
+    return response.json();
+})
+.then(person => {
+    console.log(`${person.name} works for ${person.company.name}`);
+})
+.catch(err => console.log(err));
